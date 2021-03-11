705ddec27b28e5a0106008162d272284a59ebcb7
Bassam_Alfatly <bassam.alftly@gmail.com>
2021-03-11 23:40:35 +0100

    added missing nested functions ea_bpfilter.m

--- ea_bpfilter.m
+++ ea_bpfilter.m
@@ -2,7 +2,7 @@ function ea_bpfilter(tc,TR,sampleLength)
     lp_HighCutoff=0.08;
     hp_LowCutoff=0.009;
 
-    disp('Bandpass-filtering...');
+%     disp('Bandpass-filtering...');
     % sampleFreq   = 1/TR;
     paddedLength = rest_nextpow2_one35(sampleLength); %2^nextpow2(sampleLength);
 
@@ -58,4 +58,55 @@ function ea_bpfilter(tc,TR,sampleLength)
 %         title('Bandpass filtered time series.');
 %     end
 
+
+function Result = rest_nextpow2_one35(n)
+%Compute the min length for FFT according to AFNI's algorithm, By Xiao-Wei Song
+%------------------------------------------------------------------------------------------------------------------------------
+%	Copyright(c) 2007~2010
+%	State Key Laboratory of Cognitive Neuroscience and Learning in Beijing Normal University
+%	Written by Xiao-Wei Song
+%	http://resting-fmri.sourceforge.net
+% 	<a href="Dawnwei.Song@gmail.com">Mail to Author</a>: Xiaowei Song
+%	Version=1.0;
+%	Release=20070903;
+
+if length(n)>1
+    n = cast(length(n),class(n));
 end
+if n<16
+    Result =2^nextpow2(n);
+    return;
+end
+
+limit =nextpow2(n);             %n=134, limit=8
+tbl=[2^(limit-1):2^limit];      %tbl =128, 129, ... , 256
+tbl =tbl(find(tbl>=n));          %tbl =134, 135, ... , 256
+for x=1:length(tbl)
+    Result =tbl(x);
+    [f,p]=log2(Result);
+    if ~isempty(f) & f == 0.5   %Copy from nextpow2.m
+        return;
+    end
+    if mod(Result,3*5)==0
+        y= Result /(3*5);
+        [f,p]=log2(y);
+        if ~isempty(f) & f == 0.5   %Copy from nextpow2.m
+            return;
+        end
+    end
+    if mod(Result,3)==0
+        y= Result /3;
+        [f,p]=log2(y);
+        if ~isempty(f) & f == 0.5   %Copy from nextpow2.m
+            return;
+        end
+    end
+    if mod(Result,5)==0
+        y= Result /5;
+        [f,p]=log2(y);
+        if ~isempty(f) & f == 0.5   %Copy from nextpow2.m
+            return;
+        end
+    end
+end
+Result =NaN;    % Should not reach, except when n=1
